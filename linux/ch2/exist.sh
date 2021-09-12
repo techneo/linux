dbfd6be3e761df845ce959eb05fa3fd168dbaa00
Suren <p.surendrakumar@gmail.com>
2021-09-12 22:18:12 +0530

    ch2 added

--- linux/ch2/exist.sh
+++ linux/ch2/exist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "File exist checker"
+# $# represents the number of command line arguments
 if [ "$#" -ne 1 ]; then
    echo "no arg passed"
    # dolar 0 gives "bash"
@@ -8,3 +8,17 @@ if [ "$#" -ne 1 ]; then
 
 fi
 
+# $1 is the argument to the script
+test -e $1
+# $? = 0 means the file or directory was found
+if [ "$?" -eq 0 ]; then
+   echo "file or directory found"
+elif [ "$?" -eq 1 ]; then
+   echo "file or directory not found"
+   exit 3
+else
+   echo "unknown error"
+   exit 1
+fi
+
+exit 0
